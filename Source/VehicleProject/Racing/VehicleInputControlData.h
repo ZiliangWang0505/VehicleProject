81062454f6ca874a02825a1c53fd6a2006b05268
Ziliang Wang <wangziliang05@gmail.com>
2021-02-15 19:42:10 +0800

    Google C++ Style
    遵循Google C++ Style Guide中关于命名空间的内不添加缩进和最后加入命名空间结束的说明

--- Source/VehicleProject/Racing/VehicleInputControlData.h
+++ Source/VehicleProject/Racing/VehicleInputControlData.h
@@ -8,101 +8,101 @@
 namespace Racing
 {
 
-	//-------------------------------------------------------------------
-	//					Documentation
-	//-------------------------------------------------------------------
-	// The input data require to controller a vehicle
-	class VehicleInputControlData
-	{
+//-------------------------------------------------------------------
+//					Documentation
+//-------------------------------------------------------------------
+// The input data require to controller a vehicle
+class VehicleInputControlData
+{
 
-	public:
-		//-----------------------------------------------------------------------------
-		// Public Methods:
-		VehicleInputControlData();
-		~VehicleInputControlData();
+public:
+	//-----------------------------------------------------------------------------
+	// Public Methods:
+	VehicleInputControlData();
+	~VehicleInputControlData();
 
-		inline float	GetInputSteering	() const;
-		inline float	GetInputThrottle	() const;
-		inline bool		GetInputHandBrake	() const;
+	inline float	GetInputSteering	() const;
+	inline float	GetInputThrottle	() const;
+	inline bool		GetInputHandBrake	() const;
 
-		inline void		SetInputSteering	( float InInputSteering );
-		inline void		SetInputThrottle	( float InInputThrottle );
-		inline void		SetInputHandBrake	( bool InInputHandBrake );
-
-		//-----------------------------------------------------------------------------
-		// Public Attributes:
-
-	protected:
-		//-----------------------------------------------------------------------------
-		// Protected Methods:
-
-		//-----------------------------------------------------------------------------
-		// Protected Attributes:
-
-	private:
-		//-----------------------------------------------------------------------------
-		// Private Methods:
-
-		//-----------------------------------------------------------------------------
-		// Private Attributes:
-		float InputSteering;
-		float InputThrottle;
-		bool InputHandBrake;
-	};
+	inline void		SetInputSteering	( float InInputSteering );
+	inline void		SetInputThrottle	( float InInputThrottle );
+	inline void		SetInputHandBrake	( bool InInputHandBrake );
 
 	//-----------------------------------------------------------------------------
-	//! @brief          Accessor 
-	//! @return         Get InputSteering
+	// Public Attributes:
+
+protected:
 	//-----------------------------------------------------------------------------
-	inline float VehicleInputControlData::GetInputSteering() const
-	{
-		return InputSteering;
-	}
+	// Protected Methods:
 
 	//-----------------------------------------------------------------------------
-	//! @brief          Accessor 
-	//! @return         Get InputThrottle
+	// Protected Attributes:
+
+private:
 	//-----------------------------------------------------------------------------
-	inline float VehicleInputControlData::GetInputThrottle() const
-	{
-		return InputThrottle;
-	}
+	// Private Methods:
 
 	//-----------------------------------------------------------------------------
-	//! @brief          Accessor 
-	//! @return         Get InputHandBrake
-	//-----------------------------------------------------------------------------
-	inline bool VehicleInputControlData::GetInputHandBrake() const
-	{
-		return InputHandBrake;
-	}
+	// Private Attributes:
+	float InputSteering;
+	float InputThrottle;
+	bool InputHandBrake;
+};
 
-	//-----------------------------------------------------------------------------
-	//! @brief      Accessor 
-	//! @param      Sets InInputSteering
-	//-----------------------------------------------------------------------------
-	inline void VehicleInputControlData::SetInputSteering(float InInputSteering)
-	{
-		InputSteering = InInputSteering;
-	}
-
-	//-----------------------------------------------------------------------------
-	//! @brief      Accessor 
-	//! @param      Sets InInputSteering
-	//-----------------------------------------------------------------------------
-	inline void VehicleInputControlData::SetInputThrottle(float InInputThrottle)
-	{
-		InputThrottle = InInputThrottle;
-	}
-
-	//-----------------------------------------------------------------------------
-	//! @brief      Accessor 
-	//! @param      Sets InInputSteering
-	//-----------------------------------------------------------------------------
-	inline void VehicleInputControlData::SetInputHandBrake(bool InInputHandBrake)
-	{
-		InputHandBrake = InInputHandBrake;
-	}
-
+//-----------------------------------------------------------------------------
+//! @brief          Accessor 
+//! @return         Get InputSteering
+//-----------------------------------------------------------------------------
+inline float VehicleInputControlData::GetInputSteering() const
+{
+	return InputSteering;
 }
+
+//-----------------------------------------------------------------------------
+//! @brief          Accessor 
+//! @return         Get InputThrottle
+//-----------------------------------------------------------------------------
+inline float VehicleInputControlData::GetInputThrottle() const
+{
+	return InputThrottle;
+}
+
+//-----------------------------------------------------------------------------
+//! @brief          Accessor 
+//! @return         Get InputHandBrake
+//-----------------------------------------------------------------------------
+inline bool VehicleInputControlData::GetInputHandBrake() const
+{
+	return InputHandBrake;
+}
+
+//-----------------------------------------------------------------------------
+//! @brief      Accessor 
+//! @param      Sets InInputSteering
+//-----------------------------------------------------------------------------
+inline void VehicleInputControlData::SetInputSteering(float InInputSteering)
+{
+	InputSteering = InInputSteering;
+}
+
+//-----------------------------------------------------------------------------
+//! @brief      Accessor 
+//! @param      Sets InInputSteering
+//-----------------------------------------------------------------------------
+inline void VehicleInputControlData::SetInputThrottle(float InInputThrottle)
+{
+	InputThrottle = InInputThrottle;
+}
+
+//-----------------------------------------------------------------------------
+//! @brief      Accessor 
+//! @param      Sets InInputSteering
+//-----------------------------------------------------------------------------
+inline void VehicleInputControlData::SetInputHandBrake(bool InInputHandBrake)
+{
+	InputHandBrake = InInputHandBrake;
+}
+
+} // Racing
 
